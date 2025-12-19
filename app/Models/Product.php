ddadb82d7fc1131a346072d6bdbae13e70f96a6f
samir ahmed <samirAhmed9016@gmail.com>
2025-12-19 09:07:32 +0200

    Initial commit: set up Laravel 12 API with Sanctum, Spatie Media Library, Spatie Translatable, and basic products and media

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Translatable\HasTranslations;
+
+class Product extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia, HasTranslations;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'quantity',
+    ];
+    public $translatable = ['title', 'description'];
+}
