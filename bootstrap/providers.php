0bb576ca01756959ac41f7ecbf8f4fb6309326b6
samir ahmed <samirAhmed9016@gmail.com>
2026-03-26 11:54:59 +0200

    feat: register Nova service provider and configuration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
+    App\Providers\NovaServiceProvider::class,
     App\Providers\ObserverServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
 ];
