09431117624230a2aeab7ec7b8b7d74b83b8ae06
samir ahmed <samirAhmed9016@gmail.com>
2026-04-06 16:55:59 +0200

    new  enum

---
+++ app/Enums/IdentifierLoginType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enums;
+
+enum IdentifierLoginType: string
+{
+    case PHONE    = 'phone_number';
+    case EMAIL       = 'email';
+
+    public function title()
+    {
+        return match ($this) {
+            self::PHONE       => __('Phone'),
+            self::EMAIL          => __('Email'),
+        };
+    }
+
+    public static function options()
+    {
+        return collect(self::cases())
+            ->mapWithKeys(
+                fn($item) => [ $item->value => self::from($item->value)->title() ]
+            )->toArray();
+    }
+}
