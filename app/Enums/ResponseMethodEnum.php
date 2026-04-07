218ca9f596bbdec2db808b8ff34a4a92130199fc
samir ahmed <samirAhmed9016@gmail.com>
2026-04-07 17:16:37 +0200

    add new enum

--- app/Enums/ResponseMethodEnum.php
+++ app/Enums/ResponseMethodEnum.php
@@ -1,17 +1,16 @@
 <?php
-
 namespace App\Enums;
 
 enum ResponseMethodEnum: string
 {
-    case STORE = 'store';
-    case UPDATE = 'update';
-    case DESTROY = 'destroy';
-    case SERVER_ERROR = 'server_error';
-    case INDEX = 'index';
-    case SHOW = 'show';
-    case UNPROCESSABLE = 'unprocessable';
-    case SINGLE = 'single';
-    case COLLECTION = 'collection';
-    case CUSTOM = 'custom';
+    case STORE              =   'store';
+    case UPDATE             =   'update';
+    case DESTROY            =   'destroy';
+    case SERVER_ERROR       =   'server_error';
+    case INDEX              =   'index';
+    case SHOW               =   'show';
+    case UNPROCESSABLE      =   'unprocessable';
+    case SINGLE             =   'single';
+    case COLLECTION         =   'collection';
+    case CUSTOM             =   'custom';
 }
